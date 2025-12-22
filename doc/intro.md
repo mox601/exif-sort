e92dc18a1148ff7345b86ae2c1e3c22ac2b0c354
Matteo Moci <11626+mox601@users.noreply.github.com>
2025-12-22 21:17:06 +0100

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to exif-sort/exif-sort-root
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/exif_sort/exif_sort_root_test.clj
@@ -0,0 +1,7 @@
+(ns exif-sort.exif-sort-root-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [exif-sort.exif-sort-root :as sut])) ; system under test
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
